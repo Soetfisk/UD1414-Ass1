7574612ba563defc2206958360e9f2be921fd2bb
asd <BTH@H474-0026.students.ad.bth.se>
2016-09-02 16:17:26 +0200

    done 4 now, commands funkar

--- UD1414-Ass1/main.cpp
+++ UD1414-Ass1/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char*argv[])
 	size_t numMessages = atoi(argv[5]);
 	
 	size_t msgSize;
-	if (std::strcmp("random", argv[6]))
+	if (std::strcmp("random", argv[6]) == 0)
 	{
 		msgSize = rand() % (memorySize/4) + 1;
 	}
@@ -19,12 +19,12 @@ int main(int argc, char*argv[])
 	}
 	
 
-	if (std::strcmp("producer", argv[1]))
+	if (std::strcmp("producer", argv[1]) == 0)
 	{
 		printf("produceeeeer, %d %d %d %d %d", delay, buffSize, memorySize,numMessages,msgSize);
 	}
 
-	if (std::strcmp("consumer", argv[1]))
+	if (std::strcmp("consumer", argv[1]) == 0)
 	{
 		printf("consuuuuumeer, %d %d %d %d %d", delay, buffSize, memorySize, numMessages, msgSize);
 	}
