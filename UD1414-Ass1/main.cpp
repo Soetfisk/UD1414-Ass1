62c0aa3666d09688f0c62e56a7effbcc0a2afdab
asd <BTH@H474-0026.students.ad.bth.se>
2016-09-02 16:11:43 +0200

    keeeepitup

--- UD1414-Ass1/main.cpp
+++ UD1414-Ass1/main.cpp
@@ -2,32 +2,35 @@
 
 int main(int argc, char*argv[])
 {
+	
 	DWORD delay = atoi(argv[2]);
 	size_t buffSize = atoi(argv[3]);
 	size_t memorySize = atoi(argv[4]);
 	size_t numMessages = atoi(argv[5]);
+	
 	size_t msgSize;
 	if (std::strcmp("random", argv[6]))
 	{
-		msgSize = rand() % memorySize + 1;
+		msgSize = rand() % (memorySize/4) + 1;
 	}
 	else
 	{
 		msgSize = atoi(argv[6]);
 	}
+	
 
 	if (std::strcmp("producer", argv[1]))
 	{
-		printf("produceeeeer, %d %d %d %d %d", delay, buffSize,numMessages,memorySize,msgSize);
+		printf("produceeeeer, %d %d %d %d %d", delay, buffSize, memorySize,numMessages,msgSize);
 	}
 
 	if (std::strcmp("consumer", argv[1]))
 	{
-		printf("consuuuuumeer, %d %d %d %d %d", delay, buffSize, numMessages, memorySize, msgSize);
+		printf("consuuuuumeer, %d %d %d %d %d", delay, buffSize, memorySize, numMessages, msgSize);
 	}
 
-	getchar();
-	getchar();
+	
+	return 0;
 }
 
 void producer()
